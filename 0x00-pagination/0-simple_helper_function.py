0df216a07e9e80a51e78705f460fb83323955f23
Barbara <barbeaboo@gmail.com>
2024-10-27 05:23:57 -0700

    corrections

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,12 +3,14 @@
 """
 A module that returns the range of indexes to return in a list
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """Returns the index for the range of items specified
     by the page and page_size"""
+
     start: int = (page - 1) * page_size # since the page is 1-indexed
     end: int = page*page_size
-    page_index: tuple = (start, end)
+    page_index: Tuple = (start, end)
     return page_index
