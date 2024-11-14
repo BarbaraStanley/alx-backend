462e7f2082530c4c90cca0eb533d23553410f0ea
Barbara <barbeaboo@gmail.com>
2024-11-14 10:00:48 -0800

    pycod correction for task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,7 +10,7 @@ def index_range(page: int, page_size: int) -> Tuple:
     """Returns the index for the range of items specified
     by the page and page_size"""
 
-    start: int = (page - 1) * page_size # since the page is 1-indexed
+    start: int = (page - 1) * page_size  # since the page is 1-indexed
     end: int = page*page_size
     page_index: Tuple = (start, end)
     return page_index
